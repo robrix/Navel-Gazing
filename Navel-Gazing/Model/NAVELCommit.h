671d9abcde54389f89c94280bfcf34dc3d055afd
Rob Rix <rob.rix@me.com>
2013-09-18 21:48:47 -0400

    Cleans up file headers.

--- Navel-Gazing/Model/NAVELCommit.h
+++ Navel-Gazing/Model/NAVELCommit.h
@@ -1,10 +1,4 @@
-//
-//  NAVELCommit.h
-//  Navel-Gazing
-//
-//  Created by Rob Rix on 9/17/2013.
 //  Copyright (c) 2013 Rob Rix. All rights reserved.
-//
 
 @import CoreData;
 
--- Navel-Gazing/main.m
+++ Navel-Gazing/main.m
@@ -1,12 +1,6 @@
-//
-//  main.m
-//  Navel-Gazing
-//
-//  Created by Rob Rix on 9/17/2013.
 //  Copyright (c) 2013 Rob Rix. All rights reserved.
-//
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 
 #import "NAVELAppDelegate.h"
 
