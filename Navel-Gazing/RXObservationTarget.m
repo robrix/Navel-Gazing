ad4affca404a34ec7a5709d1b9f7b0ec30754f6e
Rob Rix <rob.rix@me.com>
2013-09-21 13:35:24 -0700

    Adds observation targets, which are themselves memo dependencies.

---
+++ Navel-Gazing/RXObservationTarget.m
@@ -0,0 +1,37 @@
+//  Copyright (c) 2013 Rob Rix. All rights reserved.
+
+#import "RXObservationTarget.h"
+
+@interface RXObservationTarget ()
+
+@property (nonatomic, readonly) id object;
+@property (nonatomic, readonly) NSString *keyPath;
+
+@end
+
+@implementation RXObservationTarget
+
++(instancetype)targetWithObject:(id)object keyPath:(NSString *)keyPath {
+	return [[self alloc] initWithObject:object keyPath:keyPath];
+}
+
+-(instancetype)initWithObject:(id)object keyPath:(NSString *)keyPath {
+	if ((self = [super init])) {
+		_object = object;
+		_keyPath = [keyPath copy];
+	}
+	return self;
+}
+
+
+#pragma mark RXMemoDependency
+
+-(void)addObserver:(id)observer context:(void *)context {
+	[self.object addObserver:observer forKeyPath:self.keyPath options:0 context:context];
+}
+
+-(void)removeObserver:(id)observer context:(void *)context {
+	[self.object removeObserver:observer context:context];
+}
+
+@end
