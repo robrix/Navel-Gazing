e9f54cec82937bf95445ff77ce68f629a7e730ff
Rob Rix <rob.rix@me.com>
2013-09-21 13:36:01 -0700

    Memos can be greedy, have dependencies, and can ignore nil values.

--- Navel-Gazing/RXMemo.h
+++ Navel-Gazing/RXMemo.h
@@ -2,14 +2,32 @@
 
 @import Foundation;
 
+#import "RXMemoDependency.h"
+
 /**
  Implements an invalidatable memoized value.
  */
-@interface RXMemo : NSObject
+@interface RXMemo : NSObject <RXMemoDependency>
 
++(instancetype)memoWithTarget:(id)target ignoreNilValues:(bool)ignoresNilValues block:(id(^)(id target))block;
 +(instancetype)memoWithTarget:(id)target block:(id(^)(id target))block;
 
 @property (nonatomic, weak, readonly) id target;
+
+@property (nonatomic, readonly) bool ignoresNilValues;
+
+/**
+ If `YES`, evaluate when invalidated (greedily).
+ If `NO`, evaluate when its value is taken (lazily).
+ 
+ Defaults to `NO`.
+ */
+@property (nonatomic, getter = isGreedy) bool greedy;
+
+/**
+ An array of `RXMemoDependency`-conformant objects changes to which should invalidate this memo.
+ */
+@property (nonatomic) NSArray *dependencies;
 
 -(void)invalidate;
 
@@ -20,6 +38,8 @@
 
 /**
  Returns the value of the memo.
+ 
+ KVO-compliant.
  */
 @property (nonatomic, readonly) id value;
 
--- Navel-Gazing/RXMemo.m
+++ Navel-Gazing/RXMemo.m
@@ -5,6 +5,7 @@
 @interface RXMemo ()
 
 @property (nonatomic) bool needsEvaluation;
+@property (nonatomic, getter = isEvaluating) bool evaluating;
 
 @property (nonatomic, readonly) id(^block)(id target);
 
@@ -12,29 +13,76 @@
 
 @implementation RXMemo
 
-+(instancetype)memoWithTarget:(id)target block:(id(^)(id target))block; {
-	return [[self alloc] initWithTarget:target block:block];
++(instancetype)memoWithTarget:(id)target ignoreNilValues:(bool)ignoresNilValues block:(id(^)(id target))block {
+	return [[self alloc] initWithTarget:target ignoreNilValues:ignoresNilValues block:block];
 }
 
--(instancetype)initWithTarget:(id)target block:(id(^)(id target))block {
++(instancetype)memoWithTarget:(id)target block:(id(^)(id target))block; {
+	return [self memoWithTarget:target ignoreNilValues:YES block:block];
+}
+
+-(instancetype)initWithTarget:(id)target ignoreNilValues:(bool)ignoresNilValues block:(id(^)(id target))block {
 	if ((self = [super init])) {
 		_target = target;
+		_ignoresNilValues = ignoresNilValues;
 		_block = [block copy];
 		_needsEvaluation = YES;
 	}
 	return self;
 }
 
-
--(void)invalidate {
-	self.needsEvaluation = YES;
+-(void)dealloc {
+	self.dependencies = nil;
 }
 
 
+#pragma mark Dependencies
+
+static void *RXMemoObservationContext = &RXMemoObservationContext;
+
+-(void)setDependencies:(NSArray *)dependencies {
+	if (![_dependencies isEqual:dependencies]) {
+		for (id<RXMemoDependency> dependency in _dependencies) {
+			[dependency removeObserver:self context:RXMemoObservationContext];
+		}
+		
+		_dependencies = [dependencies copy];
+		
+		for (id<RXMemoDependency> dependency in _dependencies) {
+			[dependency addObserver:self context:RXMemoObservationContext];
+		}
+	}
+}
+
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+	if (context == RXMemoObservationContext)
+		[self invalidate];
+	else
+		[super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+}
+
+
+#pragma mark Invalidation
+
+-(void)invalidate {
+	self.needsEvaluation = YES;
+	if (self.isGreedy) {
+		[self evaluate];
+	}
+}
+
+
+#pragma mark Evaluation
+
 -(void)evaluate {
-	if (self.needsEvaluation) {
+	if (!self.isEvaluating && self.needsEvaluation) {
+		self.evaluating = YES;
+		[self willChangeValueForKey:@"value"];
 		_value = self.block(self.target);
-		self.needsEvaluation = NO;
+		[self didChangeValueForKey:@"value"];
+		
+		self.needsEvaluation = (!self.ignoresNilValues) || (_value == nil);
+		self.evaluating = NO;
 	}
 }
 
@@ -44,6 +92,17 @@
 -(id)value {
 	[self evaluate];
 	return _value;
+}
+
+
+#pragma mark RXMemoDependency
+
+-(void)addObserver:(id)observer context:(void *)context {
+	[self addObserver:observer forKeyPath:@"value" options:0 context:context];
+}
+
+-(void)removeObserver:(id)observer context:(void *)context {
+	[self removeObserver:observer forKeyPath:@"value" context:context];
 }
 
 @end
